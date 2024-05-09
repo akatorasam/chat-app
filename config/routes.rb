15e7f51a04dda4ca187470b6b4cf5e0a0ec947ec
akatorasam <rstoyo3.kikuchi.ayaka@techcamp-training.com>
2024-05-09 14:59:45 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   #get 'messages/index'
-  root to: "messages#index"
   devise_for :users
+  root to: "rooms#index"
+    #root to: "messages#index"だとトップページの表示ではなくトーク画面の表示
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
